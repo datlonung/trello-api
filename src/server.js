768f36c9828d7a1d17982dbec2331a00b27c7cfc
datlonung <hotandat645@gmail.com>
2024-07-06 21:58:02 +0700

    init code

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const hostname = "localhost";
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello</h1>");
+});
+
+app.listen(port, hostname),
+  () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  };
